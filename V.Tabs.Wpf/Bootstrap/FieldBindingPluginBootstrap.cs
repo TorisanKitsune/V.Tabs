7ccfe00b2d98dd03688ea9bf195a4da2f68959c1
Mark Kromis <mkromis@outlook.com>
2019-05-10 22:28:57 -0400

    Removed more plugins

--- V.Tabs.Wpf/Bootstrap/FieldBindingPluginBootstrap.cs
+++
@@ -1,9 +0,0 @@
-using MvvmCross.Platform.Plugins;
-
-namespace V.Tabs.Wpf.Bootstrap
-{
-    public class FieldBindingPluginBootstrap
-        : MvxPluginBootstrapAction<MvvmCross.Plugins.FieldBinding.PluginLoader>
-    {
-    }
-}
--- V.Tabs.Wpf/Bootstrap/MethodBindingPluginBootstrap.cs
+++
@@ -1,9 +0,0 @@
-using MvvmCross.Platform.Plugins;
-
-namespace V.Tabs.Wpf.Bootstrap
-{
-    public class MethodBindingPluginBootstrap
-        : MvxPluginBootstrapAction<MvvmCross.Plugins.MethodBinding.PluginLoader>
-    {
-    }
-}
